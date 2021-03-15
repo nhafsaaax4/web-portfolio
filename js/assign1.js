a882be72da69b5ebcd6e6319b402dcb7cbb8d398
Hafsa Najam <80599502+nhafsaaax4@users.noreply.github.com>
2021-03-15 17:08:47 -0400

    upload web-portfolio

---
+++ js/assign1.js
@@ -0,0 +1,38 @@
+//placeholder buttons' functions
+function function1() {
+    alert("Assignment 1 is not completed...Please wait.");
+  }
+  
+  function function2() {
+    alert("Assignment 2 is not completed...Please wait.");
+  }
+  
+  function function3() {
+    alert("Assignment 3 is not completed...Please wait.");
+  }
+  
+  function function4() {
+    alert("Assignment 4 is not completed...Please wait.");
+  }
+  
+  function function5() {
+    alert("Assignment 5 is not completed...Please wait.");
+  }
+  
+  function function6() {
+    alert("Assignment 6 is not completed...Please wait.");
+  }
+  
+  function function7() {
+    alert("Assignment 7 is not completed...Please wait.");
+  }
+  
+  function function8() {
+    alert("Assignment 8 is not completed...Please wait.");
+  }
+  
+  function function9() {
+    alert("Assignment 9 is not completed...Please wait.");
+  }
+  
+  
